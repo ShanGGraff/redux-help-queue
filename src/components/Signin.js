3782bcc365f15e4e5231451c81b8135bf56166fc
Shane Graff <copellius@gmail.com>
2022-02-06 10:15:54 -0800

    BUGFIX: Forms nows working for Signin.js

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -6,7 +6,6 @@ import firebase from "firebase/app";
 //     <h1>Sign In</h1>
 //   );
 // }
-
 function Signin(){
   function doSignUp(event) {
     event.preventDefault();
@@ -14,6 +13,25 @@ function Signin(){
     const password = event.target.password.value;
     firebase.auth().createUserWithEmailAndPassword(email, password).then(function(){
       console.log("successfully signed up!");
+    }).catch(function(error) {
+      console.log(error.message);
+    });
+  }
+
+  function doSignIn(event) {
+    event.preventDefault();
+    const email = event.target.signinEmail.value;
+    const password = event.target.signinPassword.value;
+    firebase.auth().signInWithEmailAndPassword(email, password).then(function() {
+      console.log("Successfully signed in!");
+    }).catch(function(error) {
+      console.log(error.message);
+    });
+  }
+
+  function doSignOut() {
+    firebase.auth().signOut().then(function() {
+      console.log("Successfully signed out!");
     }).catch(function(error) {
       console.log(error.message);
     });
@@ -33,10 +51,24 @@ function Signin(){
           placeholder='Password' />
         <button type='submit'>Sign up</button>
       </form>
+
+      <h1>Sign In</h1>
+      <form onSubmit={doSignIn}>
+        <input
+          type='text'
+          name='signinEmail'
+          placeholder='email' />
+        <input
+          type='password'
+          name='signinPassword'
+          placeholder='Password' />
+        <button type='submit'>Sign in</button>
+      </form>
+
+      <h1>Sign Out</h1>
+      <button onClick={doSignOut}>Sign out</button>
     </React.Fragment>
   )
-
 }
-
 
 export default Signin
