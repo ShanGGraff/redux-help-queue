bb7b3577d10576ce3b893dc7af39ef137ef4ebb6
Shane Graff <copellius@gmail.com>
2022-02-06 11:21:26 -0800

    Add styling to Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from 'styled-components';
+
+const HelpQueueHeader = styled.h1`
+  font-size: 24px;
+  text-align: center;
+  color: white;
+`;
+
+const StyledWrapper = styled.section`
+  background-color: orange;
+`;
 
 function Header(){
   return (
+    <StyledWrapper>
     <React.Fragment>
-      <h1>Help Queue</h1>
+      <HelpQueueHeader>
+        <h1>Help Queue</h1>
+      </HelpQueueHeader>
+
       <ul>
         <li>
           <Link to="/">Home</Link>
@@ -14,6 +29,7 @@ function Header(){
         </li>
       </ul>
     </React.Fragment>
+    </StyledWrapper>
   );
 }
 
